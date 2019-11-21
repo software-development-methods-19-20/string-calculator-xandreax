59a416bcc9abe39e6cd774663618e8d4575abb86
andreamjc <xandreaxbora@gmail.com>
2019-11-21 18:22:28 +0100

    Step 4 completed

--- production/source/dssc/StringCalculator.java
+++ production/source/dssc/StringCalculator.java
@@ -10,6 +10,18 @@ public class StringCalculator {
         if(numbers.isEmpty()) {
             return 0;
         }
+        else if(numbers.charAt(0)=='/'){
+            String alternativeRegex = String.valueOf(numbers.charAt(2));
+            Pattern pattern = Pattern.compile(alternativeRegex);
+            String[] tokens = pattern.split(numbers.substring(4));
+            int somma=0;
+            for (String token : tokens) {
+                if(!token.equals("")) {
+                    somma = somma + Integer.parseInt(token);
+                }
+            }
+            return somma;
+        }
         else{
             Pattern pattern = Pattern.compile(regex);
             String[] tokens = pattern.split(numbers);
--- test/source/test/AddNumbersTest.java
+++ test/source/test/AddNumbersTest.java
@@ -21,9 +21,11 @@ public class AddNumbersTest {
     void twoNumbers() { assertThat(StringCalculator.add("1,2"), is(3)); }
 
     @Test
-    void manynumbers() { assertThat(StringCalculator.add("1,2,3,4,5"), is(15)); }
+    void manyNumbers() { assertThat(StringCalculator.add("1,2,3,4,5"), is(15)); }
 
     @Test
     void newline() { assertThat(StringCalculator.add("1,2\n3,4\n1,1"), is (12));}
 
+    @Test
+    void newDelimiter() { assertThat(StringCalculator.add("//;\n1;2;3;4;1;1"), is (12));}
 }
