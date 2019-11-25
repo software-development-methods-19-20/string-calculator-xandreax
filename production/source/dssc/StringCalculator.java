d48b43abe7b794db1db5e16b56c2e1a2982a75b3
andreamjc <xandreaxbora@gmail.com>
2019-11-25 11:43:57 +0100

    Step 5 & 6 completed

--- production/source/dssc/StringCalculator.java
+++ production/source/dssc/StringCalculator.java
@@ -1,38 +1,42 @@
 package dssc;
 
 import java.util.regex.Pattern;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class StringCalculator {
 
     private static final String regex = "(,|\\R)";
 
     public static int add(String numbers) {
-        if(numbers.isEmpty()) {
-            return 0;
-        }
-        else if(numbers.charAt(0)=='/'){
-            String alternativeRegex = String.valueOf(numbers.charAt(2));
-            return splitString(numbers, alternativeRegex);
-        }
-        else{
-            return splitString(numbers, regex);
-        }
+            if (numbers.isEmpty()) {
+                return 0;
+            } else if (numbers.charAt(0) == '/') {
+                String alternativeRegex = String.valueOf(numbers.charAt(2));
+                return splitString(numbers, alternativeRegex);
+            } else {
+                return splitString(numbers, regex);
+            }
     }
+
     private static int splitString (String numbers, String regex){
         Pattern pattern = Pattern.compile(regex);
-        String[] tokens;
+        Stream<String> tokens;
         if(regex.equals("(,|\\R)")){
-            tokens = pattern.split(numbers);
+            tokens = Stream.of(pattern.split(numbers));
         }
         else{
-            tokens = pattern.split(numbers.substring(4));
+            tokens = Stream.of(pattern.split(numbers.substring(4)));
         }
-        int somma=0;
-        for (String token : tokens) {
-            if(!token.equals("")) {
-                somma = somma + Integer.parseInt(token);
+        int[] intTokens = tokens.filter(x-> !x.equals("")).mapToInt(Integer::parseInt).toArray();
+        if(IntStream.of(intTokens).anyMatch(x -> x<0)){
+            StringBuilder negatives = new StringBuilder();
+            for (int intToken : intTokens) {
+                if (intToken < 0)
+                    negatives.append(Integer.toString(intToken)).append(" ");
             }
+            throw new IllegalArgumentException("Negatives not allowed:"+negatives);
         }
-        return somma;
+        return IntStream.of(intTokens).filter(x -> x<1001).sum();
     }
 }
--- test/source/test/AddNumbersTest.java
+++ test/source/test/AddNumbersTest.java
@@ -13,9 +13,7 @@ public class AddNumbersTest {
     void emptyString() { assertThat(StringCalculator.add(""), is(0)); }
 
     @Test
-    void oneNumber() {
-        assertThat(StringCalculator.add("1"), is(1));
-    }
+    void oneNumber() { assertThat(StringCalculator.add("1"), is(1)); }
 
     @Test
     void twoNumbers() { assertThat(StringCalculator.add("1,2"), is(3)); }
@@ -28,4 +26,10 @@ public class AddNumbersTest {
 
     @Test
     void newDelimiter() { assertThat(StringCalculator.add("//;\n1;2;3;4;1;1"), is (12));}
+
+    @Test
+    void negativesNumber() throws Exception {assertThat(StringCalculator.add("-1,5,-5,5,-7"), is ("Negatives not allowed"));}
+
+    @Test
+    void numbersBiggerThan1000() { assertThat(StringCalculator.add("1,2,3,1100,1700,1665"), is(6)); }
 }
